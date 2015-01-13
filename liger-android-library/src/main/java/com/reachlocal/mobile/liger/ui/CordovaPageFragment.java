66a3180c7b995a4954c4f71835175d0fbe7ca25d
Mark Wagner <mark.wagner@reachlocal.com>
2015-01-13 15:16:04 -0600

    Stopping crash related to removed receiver in tab navigation

--- liger-android-library/src/main/java/com/reachlocal/mobile/liger/ui/CordovaPageFragment.java
+++ liger-android-library/src/main/java/com/reachlocal/mobile/liger/ui/CordovaPageFragment.java
@@ -169,7 +169,9 @@ public class CordovaPageFragment extends PageFragment implements ToolbarLayout.O
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        mWebView.handleDestroy();
+        //TODO - Leaving this commented out until I can determine best way to deal with the cordova
+        // receiver.
+        //mWebView.handleDestroy();
         if (LIGER.LOGGING) {
             Log.d(LIGER.TAG, this.getClass().getSimpleName() + ".onDestroyView() " + pageName);
         }
