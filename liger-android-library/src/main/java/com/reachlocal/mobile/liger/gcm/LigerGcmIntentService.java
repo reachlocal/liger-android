709fe6969af01114e82acb0c02783afd8346cb82
Mark Wagner <mark.wagner@reachlocal.com>
2015-01-14 11:52:48 -0600

    Cleaning up the notification code"

--- liger-android-library/src/main/java/com/reachlocal/mobile/liger/gcm/LigerGcmIntentService.java
+++ liger-android-library/src/main/java/com/reachlocal/mobile/liger/gcm/LigerGcmIntentService.java
@@ -54,30 +54,10 @@ public class LigerGcmIntentService extends IntentService {
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Log.e(LIGER.TAG, "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.e(LIGER.TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
                 sendNotification("Received: " + extras.toString(), intent);
                 Log.e(LIGER.TAG, "Received: " + extras.toString());
-            }else if ("send_event".equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Log.e(LIGER.TAG, "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.e(LIGER.TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+            }else {
                 // Post notification of received message.
                 sendNotification("Received: " + extras.toString(), intent);
                 Log.e(LIGER.TAG, "Received: " + extras.toString());
